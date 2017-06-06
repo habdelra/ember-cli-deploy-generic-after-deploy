0fb1d84c9eacdd07d5b599bffa8194d0e4a6caec
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2017-06-06 14:35:25 -0400

    initial commit;

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-/* eslint-env node */
-'use strict';
+var BasePlugin = require('ember-cli-deploy-plugin');
 
 module.exports = {
-  name: 'ember-cli-deploy-generic-after-deploy'
+  name: 'ember-cli-deploy-generic-after-deploy',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      didDeploy: function(context) {
+        this.log("Running generic after deploy callback");
+        var callback = this.readConfig('callback') || null;
+        return Promise.resolve(callback());
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
